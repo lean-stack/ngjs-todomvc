d5ffa8951f8887fabfb1caf7ee0773ada4e4eb75
Micha Alt <mail@lean-stack.de>
2019-05-14 19:08:39 +0200

    WIP: MainController. First implementation.

--- src/app/controllers/main.controller.js
+++ src/app/controllers/main.controller.js
@@ -7,15 +7,51 @@
     $scope.todos       = [];
     $scope.editedTodo  = null;
     $scope.editedTitle = ''
+    $scope.routingImplemented = false;
 
     // Calculated Properties
-    $scope.activeCount = function () {};
-    $scope.hasCompletedItems = function () {};
-    $scope.hasActiveItems = function () {};
+
+    $scope.hasTodos = function () {
+      return $scope.todos.length > 0;
+    };
+
+    $scope.activeCount = function () {
+      return $scope.todos.reduce( function (count, t) { return t.completed ? count : count +1 }, 0);
+    };
+
+    $scope.hasCompletedItems = function () {
+      // Bang! Polyfill for find/findIndex or manually:
+      for(var ix = 0; ix < $scope.todos.length; ix++) {
+        if ($scope.todos[ix].completed === true) return true;
+      }
+      return false;
+    };
+
+    $scope.hasActiveItems = function () {
+      // Bang! Polyfill for find/findIndex or manually:
+      for(var ix = 0; ix < $scope.todos.length; ix++) {
+        if ($scope.todos[ix].completed === false) return true;
+      }
+      return false;
+    };
 
     // Controller Logic
-    $scope.createTodo = function () {};
-    $scope.removeTodo = function () {};
+
+    var lastId = 0;
+    $scope.createTodo = function () {
+       // ng-model properties are trimmed by default
+      if ($scope.newTitle.length > 0) {
+        var todo = { id: ++lastId, title: $scope.newTitle, completed: false };
+        $scope.todos.push(todo);
+        $scope.newTitle = '';
+      }
+    };
+
+    $scope.removeTodo = function (todo) {
+      var todoIx = $scope.todos.indexOf(todo);
+      $scope.todos.splice(todoIx, 1);
+    };
+
     $scope.beginEdit = function () {};
     $scope.cancelEdit = function () {};
     $scope.commitEdit = function () {};
